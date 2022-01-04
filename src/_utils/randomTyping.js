c2d750cb00a550df81811ea7d5fa78fc7767eb04
Mitscherlich Wei <mitscherlich36@gmail.com>
2022-01-04 22:31:11 +0800

    feat: enhance chinese typing simulation

--- src/_utils/randomTyping.js
+++ src/_utils/randomTyping.js
@@ -1,7 +1,12 @@
 import { random } from '../_utils'
 
+const IS_CN_REGEXP = /[\u4E00-\u9FA5]/g
+
 export function randomTyping(string = '') {
-  let n = string.length / 1800
+  // increase one character for each chinese word
+  const words = string.length + string.match(IS_CN_REGEXP)?.length ?? 0
+
+  let n = words / 1800
 
   if (n < 1) {
     n = 1
